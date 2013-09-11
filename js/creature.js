111b2ba3f907eddb51875718e085949a47e7ab30
Bekir Dağ <bekir@piyote.com>
2013-09-11 12:00:16 +0300

    seperate files

--- js/creature.js
+++ js/creature.js
@@ -23,36 +23,19 @@ function go(me,x,y) {
 	me_o.style.top = y+"px";
 }
 
-function follow(me,pray) {
-	var new_position = get_position(me,pray,"get_closer");
-	go(me,new_position.x,new_position.y);
-	
-	me_o = document.getElementById(me);
-	him_o = document.getElementById(pray);
-	
-	if(me_o.offsetLeft!==him_o.offsetLeft || me_o.offsetTop!==him_o.offsetTop)
-	{
-		setTimeout('follow("'+me_o.getAttribute("id")+'","'+him_o.getAttribute("id")+'")', 1);
-	}
-	else
-	{
-		console.log(me_o.offsetLeft+","+him_o.offsetLeft+","+me_o.offsetTop+","+him_o.offsetTop);
-	}
-}
-
 function get_position(me,him,action) {
 
 	me_o = document.getElementById(me);
 	him_o = document.getElementById(him);
-	var my_step = me_o.getAttribute("step");
-	var his_step = him_o.getAttribute("step");
+	var my_speed = me_o.getAttribute("speed");
+	var his_speed = him_o.getAttribute("speed");
 
-	var him_position = $("#"+him).position();
+	var his_position = $("#"+him).position();
 	var my_position = $("#"+me).position();
 
 	
-	var x2 = my_position.left-him_position.left;
-	var y2 = my_position.top-him_position.top;
+	var x2 = my_position.left-his_position.left;
+	var y2 = my_position.top-his_position.top;
 	
 	var abs_x2 = Math.abs(x2);
 	var abs_y2 = Math.abs(y2);
@@ -91,15 +74,27 @@ function get_position(me,him,action) {
 		}
 	}
 
-
-	var final_x = new_x_way*my_step + my_position.left;
-	var final_y = new_y_way*my_step + my_position.top;
-	
-	// final_x = Math.round(final_x);
-	// final_y = Math.round(final_y);
-	
+	var final_x = new_x_way*my_speed + my_position.left;
+	var final_y = new_y_way*my_speed + my_position.top;
 	
 	return {x:final_x,y:final_y};
+}
+
+function follow(me,pray) {
+	var new_position = get_position(me,pray,"get_closer");
+	go(me,new_position.x,new_position.y);
+	
+	me_o = document.getElementById(me);
+	him_o = document.getElementById(pray);
+	
+	if(me_o.offsetLeft!==him_o.offsetLeft || me_o.offsetTop!==him_o.offsetTop)
+	{
+		setTimeout('follow("'+me_o.getAttribute("id")+'","'+him_o.getAttribute("id")+'")', 1);
+	}
+	else
+	{
+		console.log(me_o.offsetLeft+","+him_o.offsetLeft+","+me_o.offsetTop+","+him_o.offsetTop);
+	}
 }
 
 function run(me,from) {
