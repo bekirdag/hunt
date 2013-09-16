3d245a15623c4f9d16804d658931dc79be3b34cb
Bekir Dağ <bekir@piyote.com>
2013-09-16 19:28:11 +0300

    mutation added

--- js/creature.js
+++ js/creature.js
@@ -3,7 +3,23 @@ var window_h = $(window).height();
 var speed_factor = 0.2;
 var creatures = {};
 var timers = [];
-
+var settings = {
+	x:window_w, 
+	y:window_h, 
+	sex_desire:100, 
+	sex_threshold:100, 
+	store:200, 
+	store_using_threshold:200, 
+	danger_distance:20, 
+	linger_rate:100, 
+	threshold:200, 
+	speed:50, 
+	eyesightfactor:50, 
+	energy:200, 
+	danger_time: 10, 
+	patrol_threshold:200,
+	mutation_rate:0.001
+};
 
 function get_random_color() {
     var letters = '0123456789ABCDEF'.split('');
@@ -26,52 +42,52 @@ function rand_id(type)
 		return type+id;
 	}
 }
-
-function create_item(type,speed,eyesightfactor,x,y,color,family,energy,threshold,linger_rate,danger_distance,danger_time,gender,store,store_using_threshold,sex_desire,sex_threshold,patrol_threshold)
+// type,speed,eyesightfactor,x,y,color,family,energy,threshold,linger_rate,danger_distance,danger_time,gender,store,store_using_threshold,sex_desire,sex_threshold,patrol_threshold
+function create_item(data)
 {
 	var types = ["hunter","prey"];
 	var ran_num = Math.floor((Math.random()*100)+0);
 	ran_num = (ran_num>50) ? 1 : 0;
 	var ran_type = types[ran_num];
-	var ran_speed = Math.floor((Math.random()*50)+0);
-	var ran_eyesightfactor = Math.floor((Math.random()*50)+10);
+	var ran_speed = Math.floor((Math.random()*settings.speed)+0);
+	var ran_eyesightfactor = Math.floor((Math.random()*settings.eyesightfactor)+10);
 	var ran_x = Math.floor((Math.random()*window_w)+0);
 	var ran_y = Math.floor((Math.random()*window_h)+0);
 	var patrolx = 0;
 	var patroly = 0;
 	var patrolset = "false";
-	var ran_energy = Math.floor((Math.random()*100)+10);
-	var ran_threshold = Math.floor((Math.random()*100)+0);
-	var ran_danger_time = Math.floor((Math.random()*10)+1);
-	var ran_linger_rate = Math.floor((Math.random()*100)+0);
-	var ran_danger_distance = Math.floor((Math.random()*20)+0);
+	var ran_energy = Math.floor((Math.random()*settings.energy)+10);
+	var ran_threshold = Math.floor((Math.random()*settings.threshold)+0);
+	var ran_danger_time = Math.floor((Math.random()*settings.danger_time)+1);
+	var ran_linger_rate = Math.floor((Math.random()*settings.linger_rate)+0);
+	var ran_danger_distance = Math.floor((Math.random()*settings.danger_distance)+0);
 	var ran_gender = Math.floor((Math.random()*100)+0);
-	var ran_store = Math.floor((Math.random()*200)+0);
-	var ran_store_using_threshold = Math.floor((Math.random()*200)+0);
-	var sex_desire = Math.floor((Math.random()*100)+0);
-	var sex_threshold = Math.floor((Math.random()*100)+0);
+	var ran_store = Math.floor((Math.random()*settings.store)+0);
+	var ran_store_using_threshold = Math.floor((Math.random()*settings.store_using_threshold)+0);
+	var ran_sex_desire = Math.floor((Math.random()*settings.sex_desire)+0);
+	var ran_sex_threshold = Math.floor((Math.random()*settings.sex_threshold)+0);
 	var ran_patrol_threshold = Math.floor((Math.random()*ran_threshold)+0);
 	ran_gender = (ran_gender>50) ? "m" : "f";
 	var ran_color = get_random_color();
-	type = (typeof type === 'undefined') ? ran_type : type;
-	speed = (typeof speed === 'undefined') ? ran_speed : speed;
-	eyesightfactor = (typeof eyesightfactor === 'undefined') ? ran_eyesightfactor : eyesightfactor;
-	x = (typeof x === 'undefined') ? ran_x : x;
-	y = (typeof y === 'undefined') ? ran_y : y;
-	color = (typeof color === 'undefined') ? ran_color : color;
+	type = (!data.hasOwnProperty("type")) ? ran_type : data.type;
+	speed = (!data.hasOwnProperty("speed")) ? ran_speed : data.speed;
+	eyesightfactor = (!data.hasOwnProperty("eyesightfactor")) ? ran_eyesightfactor : data.eyesightfactor;
+	x = (!data.hasOwnProperty("x")) ? ran_x : data.x;
+	y = (!data.hasOwnProperty("y")) ? ran_y : data.y;
+	color = (!data.hasOwnProperty("color")) ? ran_color : data.color;
 	color = (type=="food") ? "#2AB811" : color;
-	family = (typeof family === 'undefined') ? color : family;
-	energy = (typeof energy === 'undefined') ? ran_energy : energy;
-	threshold = (typeof threshold === 'undefined') ? ran_threshold : threshold;
-	danger_time = (typeof danger_time === 'undefined') ? ran_danger_time : danger_time;
-	linger_rate = (typeof linger_rate === 'undefined') ? ran_linger_rate : linger_rate;
-	danger_distance = (typeof danger_distance === 'undefined') ? ran_danger_distance : danger_distance;
-	gender = (typeof gender === 'undefined') ? ran_gender : gender;
+	family = (!data.hasOwnProperty("family")) ? color : data.family;
+	energy = (!data.hasOwnProperty("energy")) ? ran_energy : data.energy;
+	threshold = (!data.hasOwnProperty("threshold")) ? ran_threshold : data.threshold;
+	danger_time = (!data.hasOwnProperty("danger_time")) ? ran_danger_time : data.danger_time;
+	linger_rate = (!data.hasOwnProperty("linger_rate")) ? ran_linger_rate : data.linger_rate;
+	danger_distance = (!data.hasOwnProperty("danger_distance")) ? ran_danger_distance : data.danger_distance;
+	gender = (!data.hasOwnProperty("gender")) ? ran_gender : data.gender;
 	store = 0;
-	store_using_threshold = (typeof store_using_threshold === 'undefined') ? ran_store_using_threshold : store_using_threshold;
-	sex_desire = (typeof sex_desire === 'undefined') ? ran_sex_desire : sex_desire;
-	sex_threshold = (typeof sex_threshold === 'undefined') ? ran_sex_threshold : sex_threshold;
-	patrol_threshold = (typeof patrol_threshold === 'undefined') ? ran_patrol_threshold : patrol_threshold;
+	store_using_threshold = (!data.hasOwnProperty("store_using_threshold")) ? ran_store_using_threshold : data.store_using_threshold;
+	sex_desire = (!data.hasOwnProperty("sex_desire")) ? ran_sex_desire : data.sex_desire;
+	sex_threshold = (!data.hasOwnProperty("sex_threshold")) ? ran_sex_threshold : data.sex_threshold;
+	patrol_threshold = (!data.hasOwnProperty("patrol_threshold")) ? ran_patrol_threshold : data.patrol_threshold;
 	border_radius = (type=="hunter") ? 3 : 10;
 	var ran_id = rand_id(type);
 	var danger_time_long = danger_time;
@@ -795,27 +811,41 @@ function cross_two(me,to)
 	{
 		left = 0;
 	}
+	
 	if(left>0)
 	{
-		type = creatures[rand_choose(me,to)].type;
-		speed = creatures[rand_choose(me,to)].speed;
-		eyesightfactor = creatures[rand_choose(me,to)].eyesightfactor;
-		x = my_x;
-		y = my_y;
-		color = creatures[rand_choose(me,to)].color;
-		family = creatures[rand_choose(me,to)].family;
-		energy = 150;
-		threshold = creatures[rand_choose(me,to)].threshold;
-		linger_rate = creatures[rand_choose(me,to)].linger_rate;
-		danger_distance = creatures[rand_choose(me,to)].danger_distance;
-		danger_time = creatures[rand_choose(me,to)].danger_time;
-		gender = creatures[rand_choose(me,to)].gender;
-		store = 0;
-		store_using_threshold = creatures[rand_choose(me,to)].store_using_threshold;
-		sex_desire = creatures[rand_choose(me,to)].sex_desire;
-		sex_threshold = creatures[rand_choose(me,to)].sex_threshold;
-		patrol_threshold = creatures[rand_choose(me,to)].patrol_threshold;
-		var id = create_item(type,speed,eyesightfactor,x,y,color,family,energy,threshold,linger_rate,danger_distance,danger_time,gender,store,store_using_threshold,sex_desire,sex_threshold,patrol_threshold);
+		var cr_data = {};
+		cr_data.type = creatures[rand_choose(me,to)].type;
+		cr_data.speed = creatures[rand_choose(me,to)].speed;
+		cr_data.eyesightfactor = creatures[rand_choose(me,to)].eyesightfactor;
+		cr_data.x = my_x;
+		cr_data.y = my_y;
+		cr_data.color = creatures[rand_choose(me,to)].color;
+		cr_data.family = creatures[rand_choose(me,to)].family;
+		cr_data.energy = 150;
+		cr_data.threshold = creatures[rand_choose(me,to)].threshold;
+		cr_data.linger_rate = creatures[rand_choose(me,to)].linger_rate;
+		cr_data.danger_distance = creatures[rand_choose(me,to)].danger_distance;
+		cr_data.danger_time = creatures[rand_choose(me,to)].danger_time;
+		cr_data.gender = creatures[rand_choose(me,to)].gender;
+		cr_data.store = 0;
+		cr_data.store_using_threshold = creatures[rand_choose(me,to)].store_using_threshold;
+		cr_data.sex_desire = creatures[rand_choose(me,to)].sex_desire;
+		cr_data.sex_threshold = creatures[rand_choose(me,to)].sex_threshold;
+		cr_data.patrol_threshold = creatures[rand_choose(me,to)].patrol_threshold;
+		
+		var mutation = Math.floor((Math.random()*1000)+0);
+		if(mutation*settings.mutation_rate>=1)
+		{
+			var can_mutate = ["speed","eyesightfactor","threshold","linger_rate","danger_distance","danger_time","store_using_threshold","sex_desire","sex_desire","patrol_threshold"];
+			var ran_attr = Math.floor((Math.random()*can_mutate.length)+0);
+			var mut_attr = can_mutate[ran_attr];
+			var new_attr = Math.floor((Math.random()*settings[mut_attr])+0);
+			console.log("mutated:  attr was:"+cr_data[mut_attr]+ " now:"+new_attr);
+			cr_data[mut_attr] = new_attr;
+		}
+		
+		var id = create_item(cr_data);
 	}
 }
 
